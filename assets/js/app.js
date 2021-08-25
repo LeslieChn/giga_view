da8f50d3d0239df0e60056f94645c15ff0e5ae7d
Preston Carey <pcarey1213@gmail.com>
2021-08-25 16:38:56 -0400

    working scatterchart

--- assets/js/app.js
+++ assets/js/app.js
@@ -137,7 +137,8 @@ let view_def=[{id:'treemap1', view_type:'treemap', request: req4, chart_def: cha
 {id:'line-chart1', view_type:'chart',  view_subtype:'barChart', request: req3, dropdowns:dropdowns, aliases:aliases, chart_def: chart_def, tile_config: {header: `Line Chart`, subheader: `this is a Line Chart`, height:'65vh', width:12}},
 {id:'grid1', view_type:'grid', request: req3, dropdowns:dropdowns, aliases:aliases, tile_config: {header: `Grid`,  subheader: `This is a Grid`, height:'65vh', width:12}},
 {id:'countymap1', view_type:'countymap', request: req5, dropdowns:dropdowns2, color_scheme:"?col_option", aliases:aliases,  tile_config: {header: `CountyMap`, subheader: `This is a CountyMap`, height:'65vh', width:12}},
-{id:'map', view_type:'geomap', request: req_geo, dropdowns:geodropdowns, tile_config: {header: `Map`, subheader: `Map of properties`, height:'65vh', width:12}}]
+{id:'map', view_type:'geomap', request: req_geo, dropdowns:geodropdowns, tile_config: {header: `Map`, subheader: `Map of properties`, height:'65vh', width:12}},
+{id:'scatterchart', view_type:'scatterChart', request: req_geo, dropdowns:geodropdowns, tile_config: {header: `ScatterChart`, subheader: `This is a Scatter Chart`, height:'65vh', width:12}}]
 
 
 const main_ps = new PerfectScrollbar('#main-container',
